5c6257c5956a5ac841b4c3cf12d49f513178a463
Rubinho01 <rubemkruger123@gmail.com>
2025-12-19 09:31:56 -0300

    db_name no database.js também

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const sequelize = new Sequelize(
     process.env.DB_URL, {
-         host: 'localhost', dialect: 'postgres' 
+         host: 'localhost', dialect: 'postgres', database: process.env.DB_NAME 
         }
     );
 
