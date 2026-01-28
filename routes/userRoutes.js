c84f8f1916440c5d6749f3e62488bd2e6ccfb15c
Rubinho01 <rubemkruger123@gmail.com>
2026-01-28 15:15:09 -0300

    Rota de logout + botão logout no dashboard

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
+const isLogged = require('../middlewares/isLogged')
 
 /* GET users listing. */
 router.post('/login', userController.join);
+
+router.get('/logout',  isLogged, userController.logout);
 
 module.exports = router;
